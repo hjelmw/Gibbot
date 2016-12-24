5850f3cef6a4825b877f987f1f02745f6c8d2302
M4nnogroth <william.hjelm@gmail.com>
2016-12-24 04:21:53 +0100

    Added more code

--- bot.js
+++ bot.js
@@ -4,41 +4,74 @@ const Discord = require('discord.js');
 const HashMap = require('hashmap');
 const client = new Discord.Client();
 
-//Not required but nice to have
 var permitted = new HashMap();
+var reply = '';
 
 client.on('message', message =>{
 	var list = message.content.split(' ');
 	var command = list[0];
 	var user = list[1];
 
+	var value = 0;
 
+
+	//check if permission expired
+	permitted.forEach(function(value, key) {
+		if((message.author.username + '#' + message.author.discriminator) === key){
+			//Does user still have permission
+			if(Math.floor((value-((new Date).getTime())/1000/60) <= 0)) {
+				permitted.remove(key);		
+			}
+		}
+	});
 
 	if(command === '!permit' && user != ''){
 		//Only correct user grants permissions
 		if(message.author.username === credentials.user.username 
 			&& message.author.discriminator === credentials.user.discriminator) {
-
-			permitted.set(user, (new Date).getTime());
-			message.reply('``@'+user + '`` has permission to open door for 1 hour');
+			
+			//user should be name#number
+			var usercheck = user.split('#');
+			if(usercheck[0] != '' && !isNaN(usercheck[1])) {
+				permitted.set(user, (new Date).getTime()+(60*60*1000));
+				message.reply('``@'+user + '`` Has permission to open door for 1 hour');
+			}
+			else {
+				message.reply('Not a valid user `' + user + '`');
+			}
+		}
+		else {
+			message.reply('You are not `'+credentials.user.username + '#' + credentials.user.discriminator + '`');
 		}
 	}
 
 	else if(message.content === '!permissions') {
+		message.reply('Authorized Users: ');
 		permitted.forEach(function(value, key) {
-			message.reply(key + ' : ' + value);
-		});	
+			value = Math.floor((value-((new Date).getTime()))/1000/60);
+			reply += '``'+key + ' : ' + value + ' Minutes left' + '\n';
+		});
+		//End markdown
+		reply += '``';
+		message.reply(reply);
 	}
 
 	else if(message.content === '!open') {
 		permitted.forEach(function(value, key) {
 			if((message.author.username + '#' + message.author.discriminator) === key){
-				message.reply('ok');
+				//Does user still have permission
+				if(Math.floor((value-((new Date).getTime())/1000/60)>0)) {
+					//functions.open_door(id, pwd);
+					message.reply('ok');
+				}
+				else {
+					message.reply('Permission expired `' + user + '`');
+					permitted.remove(key);
+				}
 			}
 
 			});
 		}
-		else {message.reply('You do not have permission to open the door!')}
 	})
 
 client.login('MjYyMDEyODk5MTk3ODQ1NTA0.Cz9WSA.nBlCJy9nlzD7KLOWZvG9TvrRxmo');
