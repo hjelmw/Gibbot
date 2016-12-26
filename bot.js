79406e0a28cdc4c441eee6c607e3a48c9982f0c8
M4nnogroth <william.hjelm@gmail.com>
2016-12-26 03:26:17 +0100

    Bot now uses mentions
    Also fixed an issue where bot would not properly remove entries in the
    hasmap 'permitted'

--- bot.js
+++ bot.js
@@ -5,64 +5,65 @@ const HashMap = require('hashmap');
 
 const client = new Discord.Client();
 var permitted = new HashMap();
+var list;
+var command;
+var user;
+var time;
+var reply = '';
+var value = 0;
 
 
 client.on('message', message =>{
-	var list = message.content.split(' ');
-	var command = list[0];
-	var user = list[1];
-
-	var reply = '';
-	var value = 0;
+	list = message.content.split(' ');
+	command = list[0];
+	user = list[1];
+	time = list[list.length - 1];
+	reply = ''
+	value = 0;	
 
 	//check if permission expired
 	permitted.forEach(function(value, key) {
-		if((message.author.username + '#' + message.author.discriminator) === key){
-			//Does user still have permission
-			if(Math.floor((value-((new Date).getTime())/1000/60) <= 0)) {
-				permitted.remove(key);		
-			}
+		//Does user still have permission
+		if((value-((new Date).getTime())/1000/60) <= 0) {			
+			permitted.remove(key);		
 		}
 	});
+	
+	if(command === '!permit' && user != '' && (!isNaN(time))) {
+		if(message.author.id === credentials.user.id) {
+			if (time === null) {time = 60;}
 
-	if(command === '!permit' && user != ''){
-		//Only correct user grants permissions
-		if(message.author.username === credentials.user.username 
-			&& message.author.discriminator === credentials.user.discriminator) {
-			
-			//user should be name#number
-			var usercheck = user.split('#');
-			if(usercheck[0] != '' && !isNaN(usercheck[1])) {
-				permitted.set(user, (new Date).getTime()+(60*60*1000));
-				message.reply('``@'+user + '`` Has permission to open door for 1 hour');
-			}
-			else {
-				message.reply('Not a valid user `' + user + '`');
-			}
+			message.mentions.users.map((user => {
+				permitted.set(user.id, (new Date).getTime()+(60*time*1000));
+			}));
+
+			message.reply('The following users have been granted permission: \n')
+			permitted.forEach(function(value, key) {
+				value = (value-((new Date).getTime()))/1000/60;
+				reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
+			}); 
+			//End markdown
+			message.channel.sendMessage(reply);
 		}
 		else {
-			message.reply('You are not `'+credentials.user.username + '#' + credentials.user.discriminator + '`');
+			message.reply('You are not <@' + credentials.user.id + '>');
 		}
 	}
-
 	else if(message.content === '!permissions') {
-		reply = '';
-		message.reply('Authorized Users: ');
-		reply += '`` \n\n' + '\n';
+		message.reply('Authorized users: \n');
 		permitted.forEach(function(value, key) {
 			value = Math.floor((value-((new Date).getTime()))/1000/60);
-			reply += key + ' : ' + value + ' Minutes left' + '\n';
+			reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
 		}); 
 		//End markdown
-		reply += ' ``';
 		message.channel.sendMessage(reply);
 	}
-
+	
 	else if(message.content === '!open') {
 		permitted.forEach(function(value, key) {
-			if((message.author.username + '#' + message.author.discriminator) === key){
+			if(message.author.id === key){
 				//Does user still have permission
-				if(Math.floor((value-((new Date).getTime())/1000/60)>0)) {
+				if((value-((new Date).getTime())/1000/60)>=0) {
 					//functions.open_door(id, pwd);
 					message.reply(' ok');
 				}
@@ -76,20 +77,20 @@ client.on('message', message =>{
 	}
 
 	else if(command === '!remove' && user !='') {
-		if(message.author.username === credentials.user.username 
-			&& message.author.discriminator === credentials.user.discriminator) {
+		if(message.author.id === credentials.user.id) {
 			permitted.forEach(function(value, key) {
-				if (key === user) {
-					permitted.remove(user);
-					message.reply('Ok, deleted ' + user);
+				if (key === user.id) {
+					permitted.remove(key);
+					message.reply('Ok, deleted <@' + user.id + '>');
 				}
 			});
 		}
 		else {
-			message.reply('You are not `'+credentials.user.username + '#' + credentials.user.discriminator + '`');	
+			message.reply('You are not <@' + credentials.user.id + '>');
 		}
 	}
 
 	})
+	
 
 client.login(credentials.token);
