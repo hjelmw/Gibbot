c1123634946438e8b6d37406cff70bb314058343
M4nnogroth <william.hjelm@gmail.com>
2017-01-09 04:42:21 +0100

    Commented and cleaned up code a bit

--- bot.js
+++ bot.js
@@ -2,11 +2,8 @@ const functions = require('./functions.js');
 const credentials = require('./credentials.json');
 const Discord = require('discord.js');
 const HashMap = require('hashmap');
-const dateformat = require('dateformat');
 const client = new Discord.Client();
 
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
 var permitted = new HashMap();
 var currentTime = 0;
 var list;
@@ -33,9 +30,13 @@ client.on('message', message =>{
 	});
 	
 	if(command === '!permit' && user != ''){
+
+		//Assume action was denied
 		var status = false;
+		
 		if(message.author.id === credentials.user.id) {
 
+			//No time was given
 			if (isNaN(time)) {time = 60;}
 
 			message.mentions.users.map((user => {
@@ -43,35 +44,40 @@ client.on('message', message =>{
 			}));
 
 			message.reply('The following users have been granted permission: \n');
+			
+			//Print hashmap
 			permitted.forEach(function(value, key) {
 				value = (value-((new Date).getTime()))/1000/60;
 				reply += '<@' + key + '>' + ' : ' + Math.floor(value) + ' Minutes left' + '\n';
 			}); 
-			//End markdown
+			
 			message.channel.sendMessage(reply);
+			
+			//Action was allowed
 			status = true;
 		}
 		else {
 			message.reply('You are not <@' + credentials.user.id + '>');
 		}
-
+		//Log action
 		functions.log_action(message.author.id, message.author.username, command, status);
 	}
 
 	else if(message.content === '!permissions') {
 		var status = false;
 		if(permitted.has(message.author.id)) {
+			
 			message.reply('Authorized users: \n');
 			permitted.forEach(function(value, key) {
 				value = Math.floor((value-((new Date).getTime()))/1000/60);
 				reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
 			}); 
-			//End markdown
+			
 			message.channel.sendMessage(reply);
 			status = true;
 		}
 		else {
-			message.reply('You do not have permission to do that ' + message.content);
+			message.reply('You don\'t have permission to do that!');
 		}
 
 		functions.log_action(message.author.id, message.author.username, command, status);
@@ -80,6 +86,7 @@ client.on('message', message =>{
 	else if(message.content === '!open') {
 		var status = false;
 		if((currentTime/1000) - ((new Date).getTime()/1000) <= 0 ) {
+
 			currentTime = (((new Date).getTime())+(1000*30));
 
 			if(permitted.has(message.author.id) || message.author.id === credentials.user.id){
@@ -89,74 +96,29 @@ client.on('message', message =>{
 					|| message.author.id === credentials.user.id) {
 					
 					message.reply('Ok, please wait...');
-				
-					status = true;
-					functions.log_action(message.author.id, message.author.username, command, status);	
 					
-					nightmare
-	            		.goto('https://www.chalmersstudentbostader.se/login/')
-	            		.wait('#page')
-
-	            		//Fill out credentials
-	            		.insert('input[name="log"]' , credentials.login.id)
-	            		.insert('input[name="pwd"]', credentials.login.pwd)
-	            		.click('.btn-primary')
-
-	            		//Wait until <a>öppna port</a> can be seen by nightmare
-	            		.wait('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a') 
-
-	            		//Server does not seem to grant access if wait is too low :/
-	            		.wait('#page > div > div > div > div.container > div > div.span4 > div.Box.Skugga.Label > div.BoxContent > div.f2-widget.Kontaktuppgifter > div > div.span7 > dl > dd')
-	            		
-	            		//Click on 'öppna port'
-	           			.evaluate(function() {
-	           				//Change <a> tag so it does not cause a pop-up
-	           				var link = document.querySelector('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a')
-	           				link.target = "";
-	           				return null;
-						})
-	           			.click('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a')
-	            		
-	            		.wait('#GridViewDoors_ctl08_btnOpen')
-	            		
-	            		//Open the door
-	            		.click('#GridViewDoors_ctl08_btnOpen')
-	            		.wait(5000)
-	            		.end()
-
-				        //report error
-				        .run(function(error, result) {
-				        	if (error) {
-				            	message.reply('Something went wrong: ');
-				              	message.channel.sendMessage(error);
-				                console.error(error);
-				              } else {
-								message.reply('Done');
-				              }
-				        });
-				        
-					}
-					else {
+					//Action was allowed
+					status = true;
+					
+					//Open the door
+					functions.open_door(credentials.login.id, credentials.login.pwd);				        
+				} else {
 						message.reply('Permission expired!');
 						permitted.remove(key);
-						
-						functions.log_action(message.author.id, message.author.username, command, status);	
-
 					}
 				}
 				else {
 					message.reply('You don\'t have permission to do that!');
-					functions.log_action(message.author.id, message.author.username, command, status);	
-
 				}	
 		}
 		else {
 
 			message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
-
-			functions.log_action(message.author.id, message.author.username, command, "timeout");
+			
+			//Different from false or true
+			status = "timeout";	
 		}
-
+		functions.log_action(message.author.id, message.author.username, command, status);
 	}
 
 	else if(command === '!remove' && user !='') {
@@ -165,8 +127,8 @@ client.on('message', message =>{
 
 			message.mentions.users.map((user => {
 				status = true;
-
 				permitted.remove(user.id);
+
 				message.reply('Ok, deleted <@' + user.id + '>');
 
 				functions.log_action(message.author.id, message.author.username, command, status);	
