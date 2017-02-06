d5f04c8766e85c179a7c9861b563fe8b3a4347ca
M4nnogroth <william.hjelm@gmail.com>
2017-02-06 20:05:32 +0100

    Modified !open
    WIP

--- src/bot.js
+++ src/bot.js
@@ -14,12 +14,9 @@ var reply = '';
 var value = 0;
 var BlCounter = 0;
 
-process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ' + err);
-});
 
 client.on('message', message =>{
-	//if(message.channel.id === credentials.channel.id) {
+
 		list = message.content.split(' ');
 		command = list[0];
 		user = list[1];
@@ -70,7 +67,7 @@ client.on('message', message =>{
 
 		else if(message.content === '!permissions') {
 			var status = false;
-			if(permitted.has(message.author.id)) {
+			if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
 				
 				message.reply('Authorized users: \n');
 				permitted.forEach(function(value, key) {
@@ -90,41 +87,44 @@ client.on('message', message =>{
 		
 		else if(message.content === '!open') {
 			var status = false;
-			if((currentTime/1000) - ((new Date).getTime()/1000) <= 0 ) {
 
-				currentTime = (((new Date).getTime())+(1000*30));
+			if(permitted.has(message.author.id) || message.author.id === credentials.user.id){
 
-				if(permitted.has(message.author.id) || message.author.id === credentials.user.id){
+				if(currentTime - ((new Date).getTime) <= 0)	{
+						currentTime = (new Date).getTime+(1000*30);
+					}
+				
+				//Does user still have permission
+				if(((permitted.get(message.author.id))-((new Date).getTime())/1000/60)>=0 
+					|| message.author.id === credentials.user.id) {
 					
-					//Does user still have permission
-					if(((permitted.get(message.author.id))-((new Date).getTime())/1000/60)>=0 
-						|| message.author.id === credentials.user.id) {
-						
+					
+
+					if(currentTime - ((new Date).getTime) >= 0) {
+
 						message.reply('Ok, please wait...');
 						
 						//Action was allowed
 						status = true;
 						
+						/*	
 						functions.open_door(credentials.login.id, credentials.login.pwd, function(data) {
 							//Wait for callback
 							message.channel.sendMessage(data);
 						});
-
-					} else {
-							message.reply('Permission expired!');
-							permitted.remove(key);
-						}
+						*/
 					}
-					else {
-						message.reply('You don\'t have permission to do that!');
-					}	
+
+				}
+				
+				else {
+					message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
+					status = 'timeout';
+				}	
+
 			}
 			else {
-
-				message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
-				
-				//Different from false or true
-				status = "timeout";	
+				message.reply('You are not, <@' + credentials.user.id + '>');
 			}
 			functions.log_action(message.author.id, message.author.username, command, status);
 		}
@@ -138,14 +138,12 @@ client.on('message', message =>{
 					permitted.remove(user.id);
 
 					message.reply('Ok, deleted <@' + user.id + '>');
-
-					functions.log_action(message.author.id, message.author.username, command, status);	
 				}));					
 			}
 			else {
 				message.reply('You are not <@' + credentials.user.id + '>');
-				functions.log_action(message.author.id, message.author.username, message.content, status);
 			}
+			functions.log_action(message.author.id, message.author.username, message.content, status);
 		}
 
 		else if(message.content === '!help'){		
@@ -153,23 +151,30 @@ client.on('message', message =>{
 			+ '`!permit` grants a user permission to open door. Only available to <@'+credentials.user.id+'>\n' 
 			+ '`!permissions` returns a list of permitted users. Only available to currently permitted users\n'
 			+ '`!remove` removes a user from the list of permitted users. Only available to <@'+credentials.user.id+'>\n'
-			+ '`!open` opens the door. Only available to permitted users.\n\n'
-
+			+ '`!open` opens the door. Only available to permitted users. This command will start a 30 second cooldown timer.\n\n'
+			
+			+ 'You can find the source code at https://github.com/M4nnogroth/Gibbot\n'
 			+ 'All actions are logged';
 			message.channel.sendMessage(reply);
+
+			functions.log_action(message.author.id, message.author.username, message.content, true);
 		}
 
 		else if(message.content === '!log'){
 			var status = false;
 			if(message.author.id === credentials.user.id) {
+
 				status = true;
 				message.channel.sendFile('./log.json');
-				functions.log_action(message.author.id, message.author.username, command, status);
+
 			}
 			else {
+
 				message.reply('You are not <@' + credentials.user.id + '>');
-				functions.log_action(message.author.id, message.author.username, command, status);
+
 			}
+
+			functions.log_action(message.author.id, message.author.username, command, status);
 		}
 		/*
 		else if(message.content === '90') {
@@ -183,8 +188,6 @@ client.on('message', message =>{
 
     		
 		}*/
-		
-	//}
 })
 
 	
