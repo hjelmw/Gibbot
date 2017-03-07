1e94fab0f115bdb9ec0021e12fdffb663a8dac32
M4nnogroth <william.hjelm@gmail.com>
2017-03-07 23:18:30 +0100

    Bot now only responds in certain channel

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 const functions = require('./functions.js');
+var blacklist = require('./blacklist');
 const credentials = require('../credentials.json');
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
@@ -31,6 +32,7 @@ client.on('debug', message =>{
 });
 
 client.on('message', message =>{
+	if (message.channel.id === credentials.channel.id) {
 	list = message.content.split(' ');
 	command = list[0];
 	arg1 = list[1];
@@ -50,214 +52,224 @@ if(command === '!permit' && arg1 != ''){
 	//Assume action was denied
 	var status = false;
 		
-	if(message.author.id === credentials.user.id) {
+	if(message.author.id === credentials.user.id ) {
 			//No time was given
-		if (isNaN(arg2)) {arg2 = 60;}
-
-		message.mentions.users.map((user => {
-			permitted.set(user.id, (new Date).getTime()+(60*(arg2)*1000));
-		}));
-
-		message.reply('The following users have been granted permission: \n');
-			
-		//Print hashmap
-		permitted.forEach(function(value, key) {
-			value = (value-((new Date).getTime()))/1000/60;
-			reply += '<@' + key + '>' + ' : ' + Math.floor(value) + ' Minutes left' + '\n';
-		}); 
-			
-		message.channel.sendMessage(reply);
-			
-		//Action was allowed
-		status = true;
-	}
-	else {
-		message.reply('You are not <@' + credentials.user.id + '>');
-	}
-	//Log action
-	functions.log_action(message.author.id, message.author.username, message.content, status);
-	}
-
-	else if(message.content === '!permissions') {
-		var status = false;
-		if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
-			message.reply('Authorized users: \n');
-
-			permitted.forEach(function(value, key) {
-				
-				value = Math.floor((value-((new Date).getTime()))/1000/60);		
-				reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
-			});
-
-			if(reply != null) {message.channel.sendMessage(reply);}
-			else{message.channel.sendMessage('No permitted users');}
-			status = true;
-		}
-		else {
-			//Escape ' character
-			message.reply('You don\'t have permission to do that!');
-		}
-
-		functions.log_action(message.author.id, message.author.username, command, status);
-	}
-	
-	else if(message.content === '!open') {
-		var status = false;
-			
-		if(permitted.has(message.author.id) || message.author.id === credentials.user.id){
-
-			if(currentTime - ((new Date).getTime()) <= 0)	{
-					currentTime = (new Date).getTime()+(1000*30);
-			}
-
-			else {
-				message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
-				return;
-			}
-
-			//Does user still have permission
-			if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
-
-				if(currentTime - ((new Date).getTime()) >= 0) {
-				
-					message.reply('Ok, please wait...');
-					
-					//Action was allowed
-					status = true;
-					
-					functions.open_door(credentials.login.id, credentials.login.pwd, function(data) {
-						//Wait for callback
-						message.channel.sendMessage(data);
-					});
-				}
-			}
-			
-			else {
-				message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
-				status = 'timeout';
-			}	
-
-		}
-		else {
-			message.reply('You do not have permission to do that!');
-			status = false;
-		}
-		functions.log_action(message.author.id, message.author.username, command, status);
-	}
-
-	else if(command === '!remove' && arg1 !='') {
-
-		var status = false;
-				
-		if (arg1.includes('https://www.youtube.com/watch?v=')) {
-			
-			/* TODO remove from queue */
-
-			message.reply('Ok, removed ' + arg1 + ' from the queue');
-		}
-		
-
-		else if(message.author.id === credentials.user.id) {
+			if (isNaN(arg2)) {arg2 = 60;}
 
 			message.mentions.users.map((user => {
-				status = true;
-				permitted.remove(user.id);
+				permitted.set(user.id, (new Date).getTime()+(60*(arg2)*1000));
+			}));
 
-				message.reply('Ok, deleted <@' + user.id + '>');
-			}));					
-		}
-		else {
-			message.reply('You are not <@' + credentials.user.id + '>');
-		}
-		functions.log_action(message.author.id, message.author.username, message.content, status);
-	}
-
-	else if(message.content === '!help'){		
-		var reply = 'Available commands: \n\n'
-		+ '`!permit` grants a user permission to open door. Only available to <@'+credentials.user.id+'>\n' 
-		+ '`!permissions` returns a list of permitted users. Only available to currently permitted users\n'
-		+ '`!remove` removes a user from the list of permitted users. Only available to <@'+credentials.user.id+'>\n'
-		+ '`!open` opens the door. Only available to permitted users. This command will start a 30 second cooldown timer.\n\n'
-		
-		+ 'You can find the source code at https://github.com/M4nnogroth/Gibbot\n'
-		+ 'All actions are logged';
-		message.channel.sendMessage(reply);
-
-		functions.log_action(message.author.id, message.author.username, message.content, true);
-	}
-
-	else if(message.content === '!log'){
-		var status = false;
-		if(message.author.id === credentials.user.id) {
-
+			message.reply('The following users have been granted permission: \n');
+				
+			//Print hashmap
+			permitted.forEach(function(value, key) {
+				value = (value-((new Date).getTime()))/1000/60;
+				reply += '<@' + key + '>' + ' : ' + Math.floor(value) + ' Minutes left' + '\n';
+			}); 
+				
+			message.channel.sendMessage(reply);
+				
+			//Action was allowed
 			status = true;
-			message.channel.sendFile('./log.json');
-
 		}
 		else {
-
 			message.reply('You are not <@' + credentials.user.id + '>');
-
+		}
+		//Log action
+		functions.log_action(message.author.id, message.author.username, message.content, status);
 		}
 
-		functions.log_action(message.author.id, message.author.username, command, status);
-	}
+		else if(message.content === '!permissions') {
+			var status = false;
+			if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
+				message.reply('Authorized users: \n');
 
-	else if(command === '!queue'){
-		if (!arg1) {
-			if (queue.length === 0) {
+				permitted.forEach(function(value, key) {
+					
+					value = Math.floor((value-((new Date).getTime()))/1000/60);		
+					reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
+				});
+
+				if(reply != null) {message.channel.sendMessage(reply);}
+				else{message.channel.sendMessage('No permitted users');}
+				status = true;
+			}
+			else {
+				//Escape ' character
+				message.reply('You don\'t have permission to do that!');
+			}
+
+			functions.log_action(message.author.id, message.author.username, command, status);
+		}
+		
+		else if(message.content === '!open') {
+			var status = false;
+				
+			if(permitted.has(message.author.id) || message.author.id === credentials.user.id){
+
+				if(currentTime - ((new Date).getTime()) <= 0)	{
+						currentTime = (new Date).getTime()+(1000*30);
+				}
+
+				else {
+					message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
+					return;
+				}
+
+				//Does user still have permission
+				if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
+
+					if(currentTime - ((new Date).getTime()) >= 0) {
+					
+						message.reply('Ok, please wait...');
+						
+						//Action was allowed
+						status = true;
+						
+						functions.open_door(credentials.login.id, credentials.login.pwd, function(data) {
+							//Wait for callback
+							message.channel.sendMessage(data);
+						});
+					}
+				}
+				
+				else {
+					message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
+					status = 'timeout';
+				}	
+
+			}
+			else {
+				message.reply('You do not have permission to do that!');
+				status = false;
+			}
+			functions.log_action(message.author.id, message.author.username, command, status);
+		}
+
+		else if(command === '!remove' && arg1 !='') {
+
+			var status = false;
+					
+			if (arg1.includes('https://www.youtube.com/watch?v=')) {
+				
+				/* TODO remove from queue */
+
+				message.reply('Ok, removed ' + arg1 + ' from the queue');
+			}
+			
+
+			else if(message.author.id === credentials.user.id) {
+
+				message.mentions.users.map((user => {
+					status = true;
+					permitted.remove(user.id);
+
+					message.reply('Ok, deleted <@' + user.id + '>');
+				}));					
+			}
+			else {
+				message.reply('You are not <@' + credentials.user.id + '>');
+			}
+			functions.log_action(message.author.id, message.author.username, message.content, status);
+		}
+
+		else if(message.content === '!help'){		
+			var reply = 'Available commands: \n\n'
+			+ '`!permit` grants a user permission to open door. Only available to <@'+credentials.user.id+'>\n' 
+			+ '`!permissions` returns a list of permitted users. Only available to currently permitted users\n'
+			+ '`!remove` removes a user from the list of permitted users. Only available to <@'+credentials.user.id+'>\n'
+			+ '`!open` opens the door. Only available to permitted users. This command will start a 30 second cooldown timer.\n\n'
+			
+			+ 'You can find the source code at https://github.com/M4nnogroth/Gibbot\n'
+			+ 'All actions are logged';
+			message.channel.sendMessage(reply);
+
+			functions.log_action(message.author.id, message.author.username, message.content, true);
+		}
+
+		else if(message.content === '!log'){
+			var status = false;
+			if(message.author.id === credentials.user.id) {
+
+				status = true;
+				message.channel.sendFile('./log.json');
+
+			}
+			else {
+
+				message.reply('You are not <@' + credentials.user.id + '>');
+
+			}
+
+			functions.log_action(message.author.id, message.author.username, command, status);
+		}
+
+		else if(command === '!queue'){
+			var status = false;
+			if (!arg1) {
+				if (queue.length === 0) {
+					message.reply('The playlist is currently empty, use !queue followed by a valid youtube link to add entries');
+				}
+				else{
+					reply += "Playlist: \n\n";
+					for (var i = 0; i < queue.length; i++) {
+						reply+= (i+1) + ': '+ queue[i] + '\n';
+					}
+					message.channel.sendMessage('\n' + reply);	
+				}
+			}
+
+			else if (arg1.includes('https://www.youtube.com/watch?v=')) {
+				status = true;
+				queue[queue.length] = arg1;
+				message.channel.sendMessage('<@' + message.author.id+'> ' + 'Added ' + arg1 + ' to the queue');
+				if (queue.length > 1) {
+					message.channel.sendMessage('There are currently ' + queue.length + ' entries in the playlist, use !queue without any arguments to see them');
+				}
+			}
+
+			else {
+				message.channel.sendMessage('Not a valid link, I currently only accept links of the format `https://www.youtube.com/watch?v=ID`')
+			}
+
+			functions.log_action(message.author.id, message.author.username, command, status);
+		}
+
+		else if(command === '!play'){
+			if (!message.member.voiceChannel) {
+				return;
+			}
+			var status = true;
+			channel = message.member.voiceChannel;
+
+			if (queue.length > 0) {
+					channel.join().then(connection => {
+						var current = queue.shift();
+						
+						//Initial seek and volume parameters need to be set
+						//DO NOT GO OVER 1
+						var streamOptions = { seek : 0, volume : 1}
+						
+						stream = ytdl(current, {filter : 'audioonly'});
+						
+			       		dispatcher = connection.playStream(stream, streamOptions);
+
+			       		message.channel.sendMessage('Currently playing: ' + current);
+					
+					}).catch(console.error);
+					functions.log_action(message.author.id, message.author.username, command, status);	
+			}
+			else {
 				message.reply('The playlist is currently empty, use !queue followed by a valid youtube link to add entries');
 			}
-			else{
-				reply += "Playlist: \n\n";
-				for (var i = 0; i < queue.length; i++) {
-					reply+= (i+1) + ': '+ queue[i] + '\n';
-				}
-				message.channel.sendMessage('\n' + reply);	
-			}
+
 		}
 
-		else if (arg1.includes('https://www.youtube.com/watch?v=')) {
-			queue[queue.length] = arg1;
-			message.channel.sendMessage('<@' + message.author.id+'> ' + 'Added ' + arg1 + ' to the queue');
-			if (queue.length > 1) {
-				message.channel.sendMessage('There are currently ' + queue.length + ' entries in the playlist, use !queue without any arguments to see them');
-			}
+		else if(command === '!stop') {
+			channel.leave();	
 		}
-
-		else {
-			message.channel.sendMessage('Not a valid link, I currently only accept links of the format `https://www.youtube.com/watch?v=ID`')
-		}
-
-		functions.log_action(message.author.id, message.author.username, command, status);
-	}
-
-	else if(command === '!play'){
-		channel = message.member.voiceChannel;
-
-		if (queue.length > 0) {
-			
-				channel.join().then(connection => {
-				/*
-				for(var i = 0; i< queue.length; i++){
-				*/
-					var current = queue.shift();
-					
-					//Initial seek and volume parameters need to be set
-					var streamOptions = { seek : 0, volume : 1}
-					
-					stream = ytdl(current, {filter : 'audioonly'});
-					
-		       		dispatcher = connection.playStream(stream, streamOptions);
-		       		message.channel.sendMessage('Currently playing: ' + current);
-				//}
-				
-				}).catch(console.error);
-				
-				functions.log_action(message.author.id, message.author.username, command, status);
-				
-			}
-		}
+}
 
 })
 
