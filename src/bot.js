f21e82933e576ce1da2ea8ab4e33ed545193a393
M4nnogroth <william.hjelm@gmail.com>
2017-08-05 16:43:29 +0200

    bot now uses promises

--- src/bot.js
+++ src/bot.js
@@ -5,11 +5,12 @@ const HashMap = require('hashmap');
 const client = new Discord.Client();
 const moment = require('moment');
 
-
 var permitted = new HashMap();
+//Because message changes when bot responds we need to store available commands 
+var commands = ["!permit", "!delete", "!open", "!permissions", "!help"];
 // July 9th 2017, 8:50:10 pm
 var startTime = moment();
-var requestCounter = 0;
+var requestCounter = 1;
 var currentTime = 0;
 var list;
 var command;
@@ -18,152 +19,92 @@ var time;
 var reply = '';
 var value = 0;
 
-client.on('message', message =>{
+client.on("error", (error) => {
+	Console.log("An error occured: " + error);
+});
+
+client.on("message", (message) => {
 	//Split message into command, users, time so we have them readily available
 	list = message.content.split(' ');
 	command = list[0];
 	user = list[1];
 	time = list[list.length - 1];
 	reply = '';
-	//Assume action was denied
-	var status = false;
-
 	//Check if permission expired
-	permitted.forEach(function(value, key) {
+	permitted.forEach((value, key) => {
 		//Remove expired users
-		if(((value/1000/60)-((new Date).getTime())/1000/60) <= 0) {			
-			permitted.remove(key);		
+		//TODO replace with moment
+		if (((value / 1000 / 60) - (new Date).getTime() / 1000 / 60) <= 0) permitted.remove(key);
+	});
+	var messageHandler = new Promise((resolve, reject) => {
+		if (list.length > 1 && commands.indexOf(command) > -1) { //Check that message is from user and not bot
+			if (message.author.id !== credentials.user.id) return reject("You are not <@' + credentials.user.id + '>");
+			switch (command) { //actions with parameters
+				case "!permit":
+					if (isNaN(time)) time = 60; //No time was given
+					message.mentions.users.map(user => {
+						permitted.set(user.id, (new Date).getTime() + (60 * (time) * 1000));
+					});
+					message.reply('The following users have been granted permission: \n');
+					//Print hashmap
+					permitted.forEach((value, key) => { //forEach is blocking
+						value = (value - ((new Date).getTime())) / 1000 / 60;
+						reply += '<@' + key + '>' + ' : ' + Math.floor(value) + ' Minutes left' + '\n';
+					});
+					resolve(reply);
+					//Action was allowed
+					break;
+				case "!delete":
+					message.mentions.users.map((user => {
+						permitted.remove(user.id);
+						reply += ('Ok, deleted <@' + user.id + '>');
+					}));
+					resolve(reply);
+					break;
+			}
+		} else {
+			switch (message.content) { //actions witout parameters
+				case "!open":
+					//opens needs extra check for permission
+					if (message.author.id !== credentials.user.id) return reject("You are not <@' + credentials.user.id + '>");
+					message.reply('Ok, please wait...');
+					functions.open_door(credentials.login.id, credentials.login.pwd, (data) => {
+						//Wait for callback
+						resolve(data);
+					});
+					break;
+				case "!permissions":
+					message.reply('Authorized users: \n');
+					permitted.forEach(function (value, key) {
+						value = Math.floor((value - ((new Date).getTime())) / 1000 / 60);
+						if (!isNaN) reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
+						else reply += '';
+					});
+					resolve(reply);
+					break;
+				case "!help":
+					resolve('Available commands: \n\n'
+						+ '`!permit` grants a user permission to open door. Only available to <@' + credentials.user.id + '>\n'
+						+ '`!permissions` returns a list of permitted users. Only available to currently permitted users\n'
+						+ '`!remove` removes a user from the list of permitted users. Only available to <@' + credentials.user.id + '>\n'
+						+ '`!open` opens the door. Only available to permitted users. This command will start a 30 second cooldown timer.\n\n'
+						+ 'You can find the source code at https://github.com/M4nnogroth/Gibbot\n');
+					break;
+				case "!uptime":
+					resolve("I have been online since: `"
+						+ startTime.format('MMMM Do YYYY, h:mm:ss a')
+						+ "`, about " + startTime.fromNow() + "\n"
+						+ requestCounter + " requests handled");
+					break;
+			}
 		}
 	});
-	
-	if(command === '!permit' && user != ''){
-		requestCounter++;			
-		if(message.author.id === credentials.user.id) {
-			//No time was given
-			if (isNaN(time)) {time = 60;}
-
-			message.mentions.users.map((user => {
-				permitted.set(user.id, (new Date).getTime()+(60*(time)*1000));
-			}));
-
-			message.reply('The following users have been granted permission: \n');
-				
-			//Print hashmap
-			permitted.forEach(function(value, key) {
-				value = (value-((new Date).getTime()))/1000/60;
-				reply += '<@' + key + '>' + ' : ' + Math.floor(value) + ' Minutes left' + '\n';
-			}); 
-				
-			message.channel.sendMessage(reply);
-				
-			//Action was allowed
-			status = true;
-		}else {message.reply('You are not <@' + credentials.user.id + '>');}
-	}
-
-	else if(message.content === '!permissions') {
+	messageHandler.then(res => {
 		requestCounter++;
-		if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
-			message.reply('Authorized users: \n');
-
-			permitted.forEach(function(value, key) {	
-				value = Math.floor((value-((new Date).getTime()))/1000/60);
-					
-				if(!isNaN){
-					reply += '<@' + key + '>' + ' : ' + value + ' Minutes left' + '\n';
-				}
-				else {
-					reply += '';
-				}
-			});
-
-			message.channel.sendMessage(reply);
-			status = true;
-		}
-		//Escape ' character
-		else {message.reply('You don\'t have permission to do that!');}
-	}
-		
-	else if(message.content === '!open') {
-		requestCounter++;
-		if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
-			if(currentTime - ((new Date).getTime()) <= 0)	{
-				currentTime = (new Date).getTime()+(1000*30);
-			}
-
-			else {
-				message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');
-				return;
-			}
-
-			//Does user still have permission
-			if(permitted.has(message.author.id) || message.author.id === credentials.user.id) {
-				if(currentTime - ((new Date).getTime()) >= 0) {
-
-					message.reply('Ok, please wait...');
-					//Action was allowed
-					status = true;
-					functions.open_door(credentials.login.id, credentials.login.pwd, function(data) {
-						//Wait for callback
-						message.channel.sendMessage(data);
-					});
-				}
-			}
-				
-			else {message.reply('You need to wait another: ' + Math.floor(((currentTime/1000) - ((new Date).getTime())/1000)) + ' seconds');}	
-
-		}
-			else {message.reply('You do not have permission to do that!');}
-	}
-
-	else if(command === '!remove' && user !='') {
-		requestCounter++;
-		if(message.author.id === credentials.user.id) {
-
-			message.mentions.users.map((user => {
-				status = true;
-				permitted.remove(user.id);
-
-				message.reply('Ok, deleted <@' + user.id + '>');
-			}));					
-		}
-		else {
-			message.reply('You are not <@' + credentials.user.id + '>');
-		}
-	}
-
-	else if(message.content === '!help'){	
-		requestCounter++;	
-		var reply = 'Available commands: \n\n'
-		+ '`!permit` grants a user permission to open door. Only available to <@'+credentials.user.id+'>\n' 
-		+ '`!permissions` returns a list of permitted users. Only available to currently permitted users\n'
-		+ '`!remove` removes a user from the list of permitted users. Only available to <@'+credentials.user.id+'>\n'
-		+ '`!open` opens the door. Only available to permitted users. This command will start a 30 second cooldown timer.\n\n'
-			
-		+ 'You can find the source code at https://github.com/M4nnogroth/Gibbot\n'
-		+ 'All actions are logged';
-		message.channel.sendMessage(reply);
-	}
-
-	else if(message.content === '!uptime'){		
-		requestCounter++;
-		reply = "I have been online since: `" 
-		+ startTime.format('MMMM Do YYYY, h:mm:ss a')  
-		+ "`, about " + startTime.fromNow() + "\n"
-		+ requestCounter + " requests handled";
-		message.channel.sendMessage(reply);
-	}
-	/*
-	else if(message.content === 'runninginthe90s') {
-		var channel = message.member.voiceChannel;
-
-		channel.join().then(connection => {	
-			const dispatcher = connection.playFile('./runninginthe90s.mp3');
-			}).catch(console.error);
-			
-	}*/
-})
-
-	
+		message.channel.sendMessage(res);
+	}).catch(err => {
+		console.log(err);
+	});
+});
 
 client.login(credentials.bot.token);
--- src/functions.js
+++ src/functions.js
@@ -1,51 +1,26 @@
 const Nightmare = require('nightmare');
-const jsonfile = require('jsonfile');
-const dateFormat = require('dateformat');
 const credentials = require('../credentials.json');
 
 module.exports = {
-    log_action : function (id, name, action, result) {
-
-        var now = dateFormat(new Date(), "yyyy-mm-dd, hh:MM:ss");
-                
-        var log = {
-            'date': now, 
-            'name': name, 
-            'id': id, 
-            'action': action, 
-            'result': result
-        };
-
-        jsonfile.writeFile(credentials.log.filename, log, {spaces: 4, flag: 'a'}, function (err) {
-            if(err) {
-                console.log(err);
-            }
-        });
-
-        console.log(now + " | " + name + " with id: " + id +" used command " + action + " result was " + result);
-    },
-
-
-    open_door : function(id, pwd, callback) {
-	   const nightmare = Nightmare({ show: true }); //Maybe this has to be set to true, no clue actually
-
+    open_door: function (id, pwd, callback) {
+        const nightmare = Nightmare({ show: true }); //Maybe this has to be set to true, no clue actually
         nightmare
             .goto('https://www.chalmersstudentbostader.se/login/')
             .wait('#page')
 
             //Fill out credentials
-            .insert('input[name="log"]' , credentials.login.id)
+            .insert('input[name="log"]', credentials.login.id)
             .insert('input[name="pwd"]', credentials.login.pwd)
             .click('.btn-primary')
 
             //Wait until <a>öppna port</a> can be seen by nightmare
-            .wait('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a') 
+            .wait('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a')
 
             //Server does not seem to grant access if wait is too low :/
             .wait('#page > div > div > div > div.container > div > div.span4 > div.Box.Skugga.Label > div.BoxContent > div.f2-widget.Kontaktuppgifter > div > div.span7 > dl > dd')
-                        
+
             //Click on 'öppna port'
-            .evaluate(function() {
+            .evaluate(function () {
                 //Change <a> tag so it does not cause a pop-up
                 var link = document.querySelector('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a')
                 link.target = "";
@@ -53,28 +28,28 @@ module.exports = {
             })
 
             .click('#page > div > div > div > div.container > div > div.span8 > div.row-fluid.equal > div:nth-child(2) > div > div.BoxContent > div.f2-widget.Stealth.Aptuslogin.Aptusport > a')
-                        
-            .wait('#GridViewDoors_ctl08_btnOpen')       
-            
+
+            .wait('#GridViewDoors_ctl08_btnOpen')
+
             //Actually open the door
             .click('#GridViewDoors_ctl08_btnOpen')
 
             //Wait for page to reload fully
             .wait('#GridViewDoors_ctl08_btnOpen')
-            
+
             //Then exit
             .end()
 
             //report error
-            .then(function () {  
+            .then(function () {
                 console.log('Door was opened');
                 callback("done");
-                
+
             })
-    	    .catch(function (error) {
-        	   console.error(error);
-               callback(error);
-      	    });
+            .catch(function (error) {
+                console.error(error);
+                callback(error);
+            });
 
     }
 }
